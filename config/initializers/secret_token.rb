9855606d848af9e91f7542bed69f5e3312faa573
Rakeshreddy Nagilla <rakesh@rakesh-rackspace.(none)>
2011-05-04 10:50:35 -0500

    empty rails repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustomRepository::Application.config.secret_token = '83c51f228a55d012f7ed79f5e26cae4101d6f2692004bb346c3b6bea90d5ff144841e7b78752862311cd09f1ad223bdf54e0e734365ada703e6c0a4cad9888f8'
